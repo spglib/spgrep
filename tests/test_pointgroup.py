92be190d9f2687adb19751963ac1d1ba085cad30
lan496 <kohei19950508@gmail.com>
2022-07-15 17:00:22 +0900

    Add series of point groups as solvable

--- tests/test_pointgroup.py
+++ tests/test_pointgroup.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-from spgrep.group import get_cayley_table, is_matrix_group
-from spgrep.pointgroup import pg_dataset
+from spgrep.group import get_cayley_table, get_inverse_index, is_matrix_group
+from spgrep.pointgroup import get_generators, pg_dataset
 
 
 def test_pg_dataset():
@@ -11,3 +11,21 @@ def test_pg_dataset():
             assert is_matrix_group(rotations)
             table = get_cayley_table(rotations)
             assert table.shape[0] == len(rotations)
+
+
+def test_group_chain():
+    for pg_symbol, groups in pg_dataset.items():
+        for idx, pg in enumerate(groups):
+            gens = get_generators(pg_symbol, idx)
+
+            order = len(pg)
+            table = get_cayley_table(np.array(pg))
+            inv = [get_inverse_index(table, g) for g in range(order)]
+
+            prev_size = order
+            for gen in gens:
+                # table[:gen, :gen] is normal subgroup
+                assert np.all(table[:gen, :gen] < gen)
+                for n in range(gen):
+                    assert all([table[inv[g], table[n, g]] < gen for g in range(prev_size)])
+                prev_size = gen
