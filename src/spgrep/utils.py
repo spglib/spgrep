Fix sign and more in induced representation

src/spgrep/utils.py
@@ -56,7 +56,8 @@ def nroot(z: np.complex_, n: int) -> np.complex_:
     """Return `n`-th power root of `z` with the minimum angle."""
     root = z ** (1 / n)
     r = np.absolute(root)
-    angle = np.remainder(np.angle(root), 2 * np.pi / n)
+    angle = np.angle(root)
+    angle -= np.rint(angle * n / (2 * np.pi)) * 2 * np.pi / n
     return r * np.exp(1j * angle)
 
 

tests/test_group.py
@@ -1,13 +1,11 @@
-from itertools import product
-
 import numpy as np
 
 from spgrep.group import (
+    check_cocycle_condition,
     get_factor_system_from_little_group,
     get_little_group,
     is_matrix_group,
 )
-from spgrep.utils import NDArrayComplex, NDArrayInt, ndarray2d_to_integer_tuple
 
 
 def test_is_matrix_group(C3v):
@@ -31,23 +29,3 @@ def test_get_little_group_and_factor_system(P42mnm):
         little_rotations, little_translations, kpoint
     )
     assert check_cocycle_condition(little_rotations, factor_system)
-
-
-def check_cocycle_condition(
-    rotations: NDArrayInt,
-    factor_system: NDArrayComplex,
-) -> bool:
-    if not is_matrix_group(rotations):
-        return False
-
-    rotations_int = [ndarray2d_to_integer_tuple(r) for r in rotations]
-
-    for i, j, k in product(range(len(rotations)), repeat=3):
-        jk = rotations_int.index(ndarray2d_to_integer_tuple(rotations[j] @ rotations[k]))
-        ij = rotations_int.index(ndarray2d_to_integer_tuple(rotations[i] @ rotations[j]))
-        if not np.isclose(
-            factor_system[i, jk] * factor_system[j, k], factor_system[ij, k] * factor_system[i, j]
-        ):
-            return False
-
-    return True

tests/test_utils.py
@@ -21,7 +21,7 @@ def test_ndarray2d_to_integer_tuple():
 
 def test_nroot():
     actual = nroot(-1j, 2)
-    expect = np.exp(1j * np.pi * 3 / 4)
+    expect = np.exp(-1j * np.pi * 1 / 4)
     assert np.allclose(actual, expect)
 
 