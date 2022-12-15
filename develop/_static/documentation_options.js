c370890010c54df63a2dd591f3110ae6f70798a0
lan496 <lan496@users.noreply.github.com>
2022-12-15 03:47:05 +0000

    deploy: 188c22e392c0314f1a67cd5d862acf495fe370e0

--- develop/_static/documentation_options.js
+++ develop/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+g35472c0',
+    VERSION: '0.1.dev1+g188c22e',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
